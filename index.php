10e3b67e128369c7e27b39a4b7ab813eccebc7ba
Anthony Garand <garand@garand.me>
2017-02-21 12:28:41 -0500

    Update version to 1.1

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 /*
  * Plugin Name: Copy Post to Clipboard
- * Version: 1.0
+ * Version: 1.1
  * Plugin URI: http://garand.me
  * Description: Copy post content to clipboard.
  * Author: Anthony Garand
